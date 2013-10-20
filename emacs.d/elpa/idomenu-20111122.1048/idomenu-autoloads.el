700ef17d1fec0f224b214429743bf7d8067f9970
Akshay Shekhervoldyman <voldyman666@gmail.com>
2013-10-20 22:03:21 +0530

    added emacs.d

---
+++ emacs.d/elpa/idomenu-20111122.1048/idomenu-autoloads.el
@@ -0,0 +1,28 @@
+;;; idomenu-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (idomenu) "idomenu" "idomenu.el" (21012 9472 363677
+;;;;;;  459000))
+;;; Generated autoloads from idomenu.el
+
+(autoload 'idomenu "idomenu" "\
+Switch to a buffer-local tag from Imenu via Ido.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("idomenu-pkg.el") (21012 9474 610922 602000))
+
+;;;***
+
+(provide 'idomenu-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; idomenu-autoloads.el ends here
