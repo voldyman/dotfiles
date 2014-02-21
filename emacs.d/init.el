fbbbdf2be3260ffa7309fd751eae280ca3f52e31
Akshay Shekhervoldyman <voldyman666@gmail.com>
2014-02-21 12:21:04 +0530

    Use C-Tab and C-S-Tab  to switch buffers

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -151,6 +151,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "a3d519ee30c0aa4b45a277ae41c4fa1ae80e52f04098a2654979b1ab859ab0bf" "d9639ebed5f3709f47b53e4bb8eea98a11455ab9336039cf06e9695a0233d5fb" default)))
  '(uniquify-buffer-name-style (quote post-forward) nil (uniquify)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -159,7 +160,11 @@
  ;; If there is more than one, they won't work right.
  )
 
-(windmove-default-keybindings 'meta)
+;; Use C-tab and C-S-tab to switch buffers
+(global-set-key [C-tab] 'other-window)
+(global-set-key [C-S-iso-lefttab] (lambda() ; use C-S-iso-lefttab because thats what emacs wants.
+                            (interactive)
+                            (other-window -1)))
 
 ;; set font for all windows
 (add-to-list 'default-frame-alist '(font . "Monaco-10"))
